99bd2ed8b710584400c5e4bc442a029fc5eaa0bb
Viktor Khramov <dev.999.victor@gmail.com>
2020-03-13 10:56:50 +0300

    [feat] stable release

--- index.php
+++ index.php
@@ -1,39 +1,75 @@
 <?require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");?>
+<?CJSCore::Init(["jquery"]);?>
 
-<div class="exchange-loader"></div>
-<h1>Обмен</h1>
+<h1 data-header="exchange"></h1>
 <div class="exchange">
-    <div class="tabs">
+    <div class="exchange-menu">
         <input type="radio" id="main" name="tab" checked>
-        <label for="main" class="tab">
+        <label for="main" class="exchange-menu__item">
             Обмен
         </label>
         <input type="radio" id="settings" name="tab">
-        <label for="settings" class="tab">
-            Основные настройки
-        </label>
-        <input type="radio" id="security" name="tab">
-        <label for="security" class="tab">
-            Настройки безопасности
+        <label for="settings" class="exchange-menu__item">
+            Настройки
         </label>
     </div>
-    <div class="wrapper">
-        <div class="sections">
-            <section data-section="main">
-                <!-- ajax -->
+    <div class="exchange-wrapper">
+        <div class="exchange-sections">
+
+            <section data-section="main" class="exchange-sections__section">
+                <form class="exchange-form" name="main">
+                    <div class="exchange-form__body">
+                        <div class="exchange-form-row">
+                            <span class="exchange-form-row__title">Файл в /upload/1c_catalog/</span>
+                            <select name="file" class="exchange-form-row__list">
+                                <?foreach (scandir($_SERVER['DOCUMENT_ROOT'].'/upload/1c_catalog/') as $file) :
+                                    if ($file === '.' || $file === '..') continue;
+                                    if (!preg_match('~.\.(?:xml)$~m', $file)) continue;?>
+                                    <option value="<?=$file?>"><?=$file?></option>
+                                <?endforeach;?>
+                            </select>
+                        </div>
+                        <div class="exchange-form-row">
+                            <span class="exchange-form-row__title">Тип файла</span>
+                            <select name="params" class="exchange-form-row__list">
+                                <option value="catalog">каталог / предложения</option>
+                                <option value="sale">заказы</option>
+                            </select>
+                        </div>
+                    </div>
+                    <input type="submit" class="exchange-form__submit" value="Начать обмен">
+                </form>
             </section>
-            <section data-section="settings">
-                <!-- ajax -->
+
+            <section data-section="settings" class="exchange-sections__section">
+                <form class="exchange-form" name="settings">
+                    <div class="exchange-form__body">
+                        <div class="exchange-form-row">
+                            <span class="exchange-form-row__title">Заголовок страницы</span>
+                            <input type="text" name="title" class="exchange-form-row__text" value="" autocomplete="off">
+                        </div>
+                        <div class="exchange-form-row">
+                            <span class="exchange-form-row__title">Путь к 1c_exchange.php</span>
+                            <input type="text" name="exchange_file_path" class="exchange-form-row__text" value="">
+                        </div>
+                    </div>
+                    <input type="submit" class="exchange-form__submit" value="Сохранить">
+                </form>
             </section>
-            <section data-section="security">
-                <!-- ajax -->
-            </section>
+
+            <div class="exchange-sections__loader"></div>
         </div>
-        <div class="logs">
-            логи
+        <div class="exchange-errors">
+            <!-- ajax -->
+        </div>
+        <div class="exchange-logs">
+            <div class="exchange-logs__text">
+                <!-- ajax -->
+            </div>
         </div>
     </div>
 </div>
 
-<link rel="stylesheet" href="src/main.css">
+<link rel="stylesheet" href="core/main.css">
+<script src="core/main.js"></script>
 <?require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
